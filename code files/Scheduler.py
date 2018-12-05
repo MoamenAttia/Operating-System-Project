029dcc41b039ecfd1efc07e71cf38086b05930c6
unknown <MoamenAttia@outlook.com>
2018-12-05 22:53:42 +0200

    final edition

--- code files/Scheduler.py
+++ code files/Scheduler.py
@@ -32,7 +32,9 @@ class Scheduler:
         self.processes.sort(key=lambda x: (x.arrivalTime, x.num))
         queue = self.processes.copy()
         queue.reverse()
-        lastTime = self.processes[0].arrivalTime
+        lastTime = self.switchTime
+        self.x += [0, lastTime]
+        self.y += [0, 0]
         while len(queue) > 0:
             process = queue.pop()
             if process.arrivalTime > lastTime:
@@ -55,14 +57,16 @@ class Scheduler:
             self.finishList += [process]
             curr = lastTime
             lastTime += self.switchTime
-            if self.switchTime!=0:
+            if self.switchTime != 0:
                 self.x += [curr, lastTime]
                 self.y += [0, 0]
 
     def HPF(self):  # not tested or even compiled yet
         # Sort by a Custom Property
         self.processes.sort(key=lambda x: (x.arrivalTime, x.num))
-        lastTime = self.processes[0].arrivalTime
+        lastTime = self.switchTime
+        self.x += [0, lastTime]
+        self.y += [0, 0]
         # this queue holds all processes. After the execution of a process it is deleted from this queue.
         queue = self.processes.copy()
         while len(queue) > 0:
@@ -78,22 +82,24 @@ class Scheduler:
             lastTime += hp[0].burstTime
             self.finishList += [hp[0]]  # should i use another [] ??
             # here we should assign the tat to the process. ask mo2men
-            tat = lastTime-hp[0].arrivalTime
-            self.processes[hp[0].num-1].tat = tat
-            self.processes[hp[0].num-1].weightedTAT = tat/hp[0].burstTime
+            tat = lastTime - hp[0].arrivalTime
+            self.processes[hp[0].num - 1].tat = tat
+            self.processes[hp[0].num - 1].weightedTAT = tat / hp[0].burstTime
             print("Process " + str(hp[0].num) + " ends at : " + str(lastTime))
-            self.x += [lastTime-hp[0].burstTime, lastTime]
+            self.x += [lastTime - hp[0].burstTime, lastTime]
             self.y += [hp[0].num, hp[0].num]
             lastTime += self.switchTime
             if self.switchTime != 0:
-                self.x += [lastTime-self.switchTime, lastTime]
+                self.x += [lastTime - self.switchTime, lastTime]
                 self.y += [0, 0]
             queue.remove(hp[0])
             hp.remove(hp[0])
 
     def __fillQueue(self, queue, lastTime):
         # assumptions
-        # 1 - if one process finishes its quantum and (one process or more y3ny) came at the end of execution .. i choose new first
+        # 1 - if one process finishes its quantum and (one process or more y3ny) came at the end of execution
+        # .. i choose new first
+
         # 2 - if one process during its execution .. new processes came .. they will be appended to queue ..
         # then append the current executing
         cnt = 1
@@ -102,6 +108,7 @@ class Scheduler:
                 self.visited[i] = True
                 queue.insert(cnt, self.processes[i])
                 cnt += 1
+
         if queue[0].remaining == 0:
             queue.remove(queue[0])
 
@@ -118,8 +125,14 @@ class Scheduler:
         self.visited = [False] * self.numProcesses
         self.finishList = []
         queue = []
-        lastTime = self.processes[0].arrivalTime
+        lastTime = self.switchTime
+        self.x += [0, lastTime]
+        self.y += [0, 0]
         self.processes.reverse()
+        curr = lastTime
+        lastTime = max(lastTime, self.processes[self.numProcesses - 1].arrivalTime)
+        self.x += [curr, lastTime]
+        self.y += [0, 0]
         while len(self.finishList) < self.numProcesses:
             self.__fillQueue(queue, lastTime)
             process = queue.pop()
@@ -157,7 +170,10 @@ class Scheduler:
         queue.reverse()
         readyQueue = []
         curr = 0
-        lastTime = self.processes[0].arrivalTime
+        lastTime = self.switchTime
+        self.x += [0, lastTime]
+        self.y += [0, 0]
+
         while len(queue) > 0 or len(readyQueue) > 0:
             if len(readyQueue) == 0:
                 process = queue.pop()
@@ -178,13 +194,13 @@ class Scheduler:
             readyQueue.sort(key=lambda x: (x.remaining, x.num))
 
             runningProcess = readyQueue[0]
-            runningProcess.waitingTime += lastTime-runningProcess.last
+            runningProcess.waitingTime += lastTime - runningProcess.last
             readyQueue.remove(runningProcess)
 
-            if len(queue) > 0 and runningProcess.remaining+lastTime >= queue[len(queue)-1].arrivalTime:
+            if len(queue) > 0 and runningProcess.remaining + lastTime >= queue[len(queue) - 1].arrivalTime:
                 curr = lastTime
-                runningProcess.last = queue[len(queue)-1].arrivalTime
-                lastTime = queue[len(queue)-1].arrivalTime
+                runningProcess.last = queue[len(queue) - 1].arrivalTime
+                lastTime = queue[len(queue) - 1].arrivalTime
                 runningProcess.remaining -= (lastTime - curr)
 
                 self.x += [curr, lastTime]
@@ -202,20 +218,17 @@ class Scheduler:
                         readyQueue.append(x)
                         queue.remove(x)
                 readyQueue.sort(key=lambda x: (x.remaining, x.num))
-                if readyQueue[0] == runningProcess:
-                    continue
-                else:
-                    curr = lastTime
-                    lastTime += self.switchTime
-                    if self.switchTime != 0:
-                        self.x += [curr, lastTime]
-                        self.y += [0, 0]
-                    continue
+                curr = lastTime
+                lastTime += self.switchTime
+                if self.switchTime != 0:
+                    self.x += [curr, lastTime]
+                    self.y += [0, 0]
+                continue
             else:
                 curr = lastTime
                 lastTime += runningProcess.remaining
                 runningProcess.last = lastTime
-                runningProcess.tat = runningProcess.last-runningProcess.arrivalTime
+                runningProcess.tat = runningProcess.last - runningProcess.arrivalTime
                 runningProcess.weightedTAT = runningProcess.tat / runningProcess.burstTime
                 self.finishList.append(runningProcess)
                 self.x += [curr, lastTime]
@@ -227,6 +240,81 @@ class Scheduler:
                     self.x += [curr, lastTime]
                     self.y += [0, 0]
                 continue
+
+    def SRTN2(self):
+        self.processes.sort(key=lambda x: (x.arrivalTime, x.num))  # Sort by a Custom Property
+        lastTime = self.processes[0].arrivalTime
+        queue = self.processes.copy()  # this queue holds all processes. After the execution of a process it is deleted from this queue.
+        previousExecutingProcess = self.processes[0].num
+        infinityRemainingProcess = Process(-1, 10000000, 0, 0)
+        queue.append(infinityRemainingProcess)
+        arrived = []
+        tat = 0
+        while len(queue) > 0 or len(arrived) > 0:
+            arrived += [process for process in queue if process.arrivalTime <= lastTime]
+            if len(arrived) == 0:
+                if len(queue) == 1:
+                    return
+
+                self.x += [lastTime, lastTime + queue[0].arrivalTime]
+                self.y += [0, 0]
+                lastTime = queue[0].arrivalTime
+                continue
+
+            queue = [process for process in queue if process.arrivalTime > lastTime]
+            # no need to initialize the remaining time it's already initialized in the constructor
+            arrived.sort(key=lambda x: (x.remaining, x.num))  # Sort processes by remaining time
+            curr = lastTime
+            if arrived[0].remaining <= queue[0].arrivalTime - lastTime:
+                switched = 0
+                if arrived[0].num != previousExecutingProcess:
+                    lastTime += self.switchTime
+                    if self.switchTime != 0:
+                        self.x += [lastTime - self.switchTime, lastTime]
+                        self.y += [0, 0]
+                        print("process num " + str(arrived[0].num) + " switched")
+                        switched = 1
+                    curr = lastTime
+
+                lastTime += arrived[0].remaining
+                tat = lastTime - arrived[0].arrivalTime
+                self.processes[arrived[0].num - 1].tat = tat
+                self.processes[arrived[0].num - 1].weightedTAT = tat / arrived[0].burstTime
+                self.processes[arrived[0].num - 1].waitingTime = tat - arrived[0].burstTime
+                self.finishList += [arrived[0]]  # another [] ??
+                print("Process " + str(arrived[0].num) + " ends at : " + str(lastTime))
+                self.x += [curr, lastTime]
+                self.y += [arrived[0].num, arrived[0].num]
+                previousExecutingProcess = arrived[0].num
+                '''lastTime += self.switchTime
+                if self.switchTime != 0:
+                    self.x += [lastTime-self.switchTime, lastTime]
+                    self.y += [0, 0]
+                    print ("process num "+ str(arrived[0].num)+" switched")'''
+                arrived.remove(arrived[0])
+                # continue
+            else:
+                switched = 0
+                if arrived[0].num != previousExecutingProcess:
+                    lastTime += self.switchTime
+                    if self.switchTime != 0:
+                        self.x += [lastTime - self.switchTime, lastTime]
+                        self.y += [0, 0]
+                        print("process num " + str(arrived[0].num) + " switched")
+                        switched = 1
+                    curr = lastTime
+                arrived[0].remaining -= queue[0].arrivalTime - lastTime
+                lastTime = queue[0].arrivalTime
+                if switched == 1:
+                    self.x += [curr, lastTime + self.switchTime]
+                    lastTime += self.switchTime
+                else:
+                    self.x += [curr, lastTime]
+                self.y += [arrived[0].num, arrived[0].num]
+                previousExecutingProcess = arrived[0].num
+
+            if len(queue) == 1 and len(arrived) == 0:
+                return
 
     def printInfo(self):
         TAT = 0
@@ -254,94 +342,24 @@ class Scheduler:
 
 
 def main():
-    scheduler = Scheduler(
-        [
-            Process(1, 0, 7, 1),
-            Process(2, 1, 5, 2),
-            Process(3, 2, 3, 3),
-            Process(4, 3, 1, 5),
-            Process(5, 4, 2, 4),
-            Process(6, 5, 1, 4),
-        ], 2, 2)
-    scheduler.SRTN()
+    scheduler = Scheduler([
+        Process(1, 5, 3, 1),
+        Process(2, 6, 8, 1),
+        Process(3, 8, 9, 1),
+    ], 2, 2)
+    # scheduler = Scheduler(
+    #     [
+    #         Process(1, 4, 7, 1),
+    #         Process(2, 5, 5, 2),
+    #         Process(3, 6, 3, 3),
+    #         Process(4, 7, 1, 5),
+    #         Process(5, 8, 2, 4),
+    #         Process(6, 9, 1, 4),
+    #     ], 2, 2)
+    scheduler.RR()
     scheduler.printInfo()
     scheduler.drawGraph()
 
 
 if __name__ == '__main__':
     main()
-
-
-# def SRTN(self):
-#        self.processes.sort(key=lambda x: (x.arrivalTime, x.num))  # Sort by a Custom Property
-#        lastTime = self.processes[0].arrivalTime
-#        queue = self.processes.copy() # this queue holds all processes. After the execution of a process it is deleted from this queue.
-#        previousExecutingProcess = self.processes[0].num
-#        infinityRemainingProcess= Process(-1,10000000,0,0)
-#        queue.append(infinityRemainingProcess)
-#        arrived=[]
-#        tat=0
-#        while len(queue)>0 or len(arrived)>0:
-#            arrived += [process for process in queue if process.arrivalTime<=lastTime]
-#            if len(arrived)==0:
-#                if len(queue)==1:
-#                    return
-#
-#                self.x+=[lastTime,lastTime+queue[0].arrivalTime]
-#                self.y+=[0,0]
-#                lastTime=queue[0].arrivalTime
-#                continue
-
-#            queue=[process for process in queue if process.arrivalTime>lastTime]
-#            # no need to initialize the remaining time it's already initialized in the constructor
-#            arrived.sort(key=lambda x: (x.remaining, x.num))  # Sort processes by remaining time
-#            curr=lastTime
-#            if arrived[0].remaining<=queue[0].arrivalTime-lastTime:
-#                switched=0
-#                if arrived[0].num !=previousExecutingProcess:
-#                    lastTime += self.switchTime
-#                    if self.switchTime != 0:
-#                        self.x += [lastTime-self.switchTime, lastTime]
-#                        self.y += [0, 0]
-#                        print ("process num "+ str(arrived[0].num)+" switched")
-#                        switched=1
-#                    curr=lastTime
-
-#                lastTime+=arrived[0].remaining
-#                tat=lastTime-arrived[0].arrivalTime
-#                self.processes[arrived[0].num-1].tat=tat
-#                self.processes[arrived[0].num-1].weightedTAT=tat/arrived[0].burstTime
-#                self.processes[arrived[0].num-1].waitingTime=tat-arrived[0].burstTime
-#                self.finishList+= [arrived[0]] # another [] ??
-#                print("Process " + str(arrived[0].num) + " ends at : " + str(lastTime))
-#                self.x+=[curr,lastTime]
-#                self.y+=[arrived[0].num,arrived[0].num]
-#                previousExecutingProcess=arrived[0].num
-#                '''lastTime += self.switchTime
-#                if self.switchTime != 0:
-#                    self.x += [lastTime-self.switchTime, lastTime]
-#                    self.y += [0, 0]
-#                    print ("process num "+ str(arrived[0].num)+" switched")'''
-#                arrived.remove(arrived[0])
-#                # continue
-#            else:
-#                switched=0
-#                if arrived[0].num !=previousExecutingProcess:
-#                    lastTime += self.switchTime
-#                    if self.switchTime != 0:
-#                        self.x += [lastTime-self.switchTime, lastTime]
-#                        self.y += [0, 0]
-#                        print ("process num "+ str(arrived[0].num)+" switched")
-#                        switched=1
-#                    curr=lastTime
-#                arrived[0].remaining-=queue[0].arrivalTime-lastTime
-#                lastTime=queue[0].arrivalTime
-#                if switched==1:
-#                    self.x+=[curr,lastTime+self.switchTime]
-#                    lastTime+=self.switchTime
-#               else :
-#                   self.x+=[curr,lastTime]
-#               self.y+=[arrived[0].num,arrived[0].num]
-#               previousExecutingProcess=arrived[0].num
-#        if len(queue) == 1 and len(arrived) == 0:
-#            return
